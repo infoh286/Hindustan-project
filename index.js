85048580d928bf319c3543c7788deca364aa216e
Ashwani <info.hcl00@gmail.com>
2025-07-07 13:43:59 +0530

    initial status

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
-app.get('/', (req, res) => {
+app.get('/registration-form', (req, res) => {
   res.render('index');
 });
 
